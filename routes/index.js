82c5d9efb9e07669cbaec91b03e9d3be9c4355a9
José Francisco Aguirre Villarroel <jofravimusic@byui.edu>
2022-05-21 12:43:50 -0300

    users route added

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ routes.get('/', (req, res) => {
   res.send('Welcome to this To Do List API');
 });
 routes.use('/todo', require('./toDoList'));
+routes.use('/users', require('./users'));
 routes.use('/api-docs', require('./doc'));
 
 module.exports = routes;
