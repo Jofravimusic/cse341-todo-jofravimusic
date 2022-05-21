9f5a90ecf31a8274c3dd9fdcfb31b63acf336cce
José Francisco Aguirre Villarroel <jofravimusic@byui.edu>
2022-05-21 10:54:18 -0300

    first files created

---
+++ server.js
@@ -0,0 +1,19 @@
+// Server Variables
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+//const dbconnection = require('./model/dbconnection');
+
+//dbconnection.connectDatabase();
+
+//app.use(bodyParser.json()).use('/', require('./routes'));
+
+app.get('/', (req, res) => {
+  res.send('Hello to this To Do List App.');
+});
+
+app.listen(port, () => {
+  console.log(`Running on Port ${port}`);
+});
