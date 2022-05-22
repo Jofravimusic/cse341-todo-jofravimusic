40489f470ee7072200ad01d765ea83f762a5303e
José Francisco Aguirre Villarroel <jofravimusic@byui.edu>
2022-05-21 22:49:28 -0300

    Last changes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const routes = require('express').Router();
 
 routes.get('/', (req, res) => {
-  res.send('Welcome to this To Do List API');
+  res.send(`Welcome to this To Do List API. Use this routes: /todo and /users. More information in /api-docs
+  `);
 });
 routes.use('/todo', require('./toDoList'));
 routes.use('/users', require('./users'));
